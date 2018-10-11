a2287a44b253afa4b8f98c6e620e854f351eecfd
Right Hand Abacus <righthandabacus@users.noreply.github.com>
2018-10-10 22:40:34 -0400

    Restructure to bootstrap4

--- js/main.js
+++ js/main.js
@@ -4,19 +4,19 @@ var main = {
 	init : function() {
 		// Minimise navbar after scrolling a little bit down
 		$(window).scroll(function() {
-			if ($(".navbar-fixed-top").offset().top > 50) {
-				$(".navbar-fixed-top").addClass("navbar-minimised");
+            if ($(".fixed-top").offset().top > 50) {
+				$(".fixed-top").addClass("navbar-minimised");
 			} else {
-				$(".navbar-fixed-top").removeClass("navbar-minimised");
+				$(".fixed-top").removeClass("navbar-minimised");
 			}
 		});
 
 		// On mobile, hide the avatar when expanding the navbar menu
 		$('#main-navbar').on('show.bs.collapse', function () {
-			$(".navbar-fixed-top").addClass("navbar-expanded");
+			$(".fixed-top").addClass("navbar-expanded");
 		});
 		$('#main-navbar').on('hidden.bs.collapse', function () {
-			$(".navbar-fixed-top").removeClass("navbar-expanded");
+			$(".fixed-top").removeClass("navbar-expanded");
 		});
 	}
 };
